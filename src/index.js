2f70e2cad66197c8e4181a4de2e84c1e427683fd
johnnyurosevic <johnnyurosevic@g.ucla.edu>
2020-06-05 22:49:55 -0700

    Initial Skeleton Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Blackjack from './Blackjack';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Blackjack />
   </React.StrictMode>,
   document.getElementById('root')
 );
