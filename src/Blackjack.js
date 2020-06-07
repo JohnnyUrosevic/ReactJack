e588733977e7b1f4359d31415b282e4d19343b32
johnnyurosevic <johnnyurosevic@g.ucla.edu>
2020-06-06 23:58:49 -0700

    Hit button

--- src/Blackjack.js
+++ src/Blackjack.js
@@ -1,18 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Blackjack.css'
 import Player from './Player.js';
 
-function Blackjack() {
-  const sample_cards = [{suit: "S", value: "A"}, {suit: "C", value: "2"}, {suit: "H", value: "10"}];
+const generate_card = () => {
+  const suits = ["C", "S", "D", "H"];
+  const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "K", "Q", "J", "A"];
+
+  const card = {
+    suit: suits[Math.floor(Math.random() * suits.length)],
+    value: values[Math.floor(Math.random() * values.length)],
+  }
+  return card;
+}
+
+export const get_value = (hand) => {
+  let hand_values = [0];
+
+  for (let card of hand) {
+    switch (card.value) {
+        case "A":
+            let low = hand_values.map((v) => v + 1);
+            let high = hand_values.map((v) => v + 11);
+            
+            hand_values = low.concat(high);
+            break;
+        case "K":
+        case "Q":
+        case "J":
+            hand_values = hand_values.map((v) => v + 10);
+            break;
+        default:
+            hand_values = hand_values.map((v) => v + Number(card.value));
+    }
+  }
+
+  const non_busted = hand_values.filter((v) => v < 22);
+
+  if (!(non_busted.length == 0)) {
+      hand_values = non_busted;
+  }
+
+  return Math.max(...hand_values);
+}
+
+const Blackjack = () => {
+  const [player_hand, set_player_hand] = useState([generate_card()])
+  const [dealer_hand, set_dealer_hand] = useState([generate_card()])
+ 
+  const [player_busted, set_player_busted] = useState(false);
+  const [dealer_busted, set_dealer_busted] = useState(false);
+
+  const [player_turn, set_player_turn] = useState(true);
+
+  useEffect(() => {
+    const set_busted = (hand, set_busted) => {
+      if (get_value(player_hand) > 21) {
+          set_busted(true);
+      }
+      else {
+        set_busted(false);
+      }
+    }
+
+    set_busted(player_hand, set_player_busted);
+    set_busted(dealer_hand, set_dealer_busted);
+  }, [player_hand]);
+
+  const handle_hit = (hand, set_hand) => {
+    set_hand(hand.concat(generate_card()));
+  }
+
   return (
     <>
       <div className="dealer">
-        <Player cards={[{suit: "S", value: "A"}]}/>
+        <Player cards={dealer_hand}/>
       </div>
       <div className="players">
-        <Player cards={[{suit: "S", value: "A"}]} />
-        <Player cards={sample_cards}/>
+        <Player cards={player_hand}/>
       </div>
+      {!player_busted && <div className="buttons">
+          <button onClick={() => handle_hit(player_hand, set_player_hand)}>Hit</button>
+          <button>Stand</button>
+      </div>}
     </>
   );
 }
--- src/Player.js
+++ src/Player.js
@@ -1,39 +1,17 @@
 import React from 'react';
 import Card from './Card.js'
 import './Player.css'
+import { get_value } from './Blackjack.js';
 
 const Player = (props) => {
-    let hand_values = [0];
-
-    for (let card of props.cards) {
-        switch (card.value) {
-            case "A":
-                let low = hand_values.map((v) => v + 1);
-                let high = hand_values.map((v) => v + 11);
-                
-                hand_values = low.concat(high);
-                break;
-            case "K":
-            case "Q":
-            case "J":
-                hand_values = hand_values.map((v) => v + 10);
-                break;
-            default:
-                hand_values = hand_values.map((v) => v + Number(card.value));
-        }
-    }
-
-    const non_busted = hand_values.filter((v) => v < 22);
-
-    if (!(non_busted.length == 0)) {
-        hand_values = non_busted;
-    }
-
     const card_to_html = (card, i) => <Card key={i} style={{marginTop: i * 2 + "%"}} suit={card.suit} value={card.value} />
     const card_html = props.cards.map(card_to_html);
+
+    const text_color = (get_value(props.cards) < 22) ? "white" : "red";
+
     return (
         <div className="cards">
-            {props.cards.length > 0 && <p>{Math.max(...hand_values)}</p>}
+            {props.cards.length > 0 && <p style={{color: text_color}}>{get_value(props.cards)}</p>}
             {card_html}
         </div>
     );
