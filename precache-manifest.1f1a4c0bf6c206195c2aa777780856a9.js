ef849357af507949d855e77fa8ad6030af969ab3
johnnyurosevic <johnnyurosevic@g.ucla.edu>
2020-06-07 22:43:50 -0700

    Updates

---
+++ precache-manifest.1f1a4c0bf6c206195c2aa777780856a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3bc14a83339616c1e113d56fd57b1ad",
+    "url": "/ReactJack/index.html"
+  },
+  {
+    "revision": "6a13243a0acc495ad653",
+    "url": "/ReactJack/static/css/main.80f6785c.chunk.css"
+  },
+  {
+    "revision": "efe083190e507f7f1a01",
+    "url": "/ReactJack/static/js/2.00acf68c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJack/static/js/2.00acf68c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a13243a0acc495ad653",
+    "url": "/ReactJack/static/js/main.ae935a25.chunk.js"
+  },
+  {
+    "revision": "cd609f1bcddfae3b2c4c",
+    "url": "/ReactJack/static/js/runtime-main.deabf041.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.53dba4353ef91fc08332717fc1873982.js"
+  "/ReactJack/precache-manifest.1f1a4c0bf6c206195c2aa777780856a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactJack/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
